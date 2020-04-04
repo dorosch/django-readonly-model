a4f21fed4fe9224c34f0b052114066a5ceaed702
Andrey Doroschenko <dorosch.github.io@yandex.ru>
2020-04-04 22:56:12 +0300

    Rename license file

--- readonly_model/__init__.py
+++ readonly_model/__init__.py
@@ -1,1 +1,7 @@
+__version__ = '1.0'
+__license__ = 'MIT'
+__author__ = 'Andrey Doroschenko'
+__email__ = 'dorosch.github.io@yandex.ru'
+__url__ = 'https://github.com/dorosch/django-readonly-model'
+
 default_app_config = 'readonly_model.apps.ReadOnlyModelAppConfig'
